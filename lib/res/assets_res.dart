a7ca93962209af334e2e150e91ca7bef1f24b0c6
Story5 <337995545@qq.com>
2023-09-14 14:20:51 +0800

    Create assets_res.dart

---
+++ lib/res/assets_res.dart
@@ -0,0 +1,10 @@
+/// Generated file. Do not edit.
+
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: lines_longer_than_80_chars
+class AssetsRes {
+  static const String PLUGIN_NAME = 'flutter_hlc';
+  static const String PLUGIN_VERSION = '1.0.0+1';
+  static const String COFFEE = 'assets/images/coffee.jpg';
+  static const String SCREEN_SHOT = 'assets/images/screen_shot.jpg';
+}
